94a7cf051a560445ce2efaf63b594070cd54f212
Tom Blaymire <thomasblaymire@gmail.com>
2023-01-18 19:18:41 +0000

    feat(app): create re-usable type interfaces

--- client/types/index.ts
+++ client/types/index.ts
@@ -16,3 +16,68 @@ export enum PROVIDERS {
   EMAIL = 'email',
   GOOGLE = 'google',
 }
+
+export interface Competition {
+  id: number
+  area: Area
+  name: string
+  code: string
+  type: string
+  emblem: string
+  plan: string
+  currentSeason: CurrentSeason
+  numberOfAvailableSeasons: number
+  lastUpdated: Date
+}
+
+export interface Area {
+  id: number
+  name: string
+  code: string
+  flag: string
+}
+
+export interface CurrentSeason {
+  id: number
+  startDate: string
+  endDate: string
+  currentMatchday: number
+  winner?: any
+}
+
+export interface Standings {
+  position: number
+  team: Team
+  playedGames: number
+  form: string
+  won: number
+  draw: number
+  lost: number
+  points: number
+  goalsFor: number
+  goalsAgainst: number
+  goalDifference: number
+}
+
+export interface Team {
+  id: number
+  name: string
+  shortName: string
+  tla: string
+  crest: string
+}
+
+export enum LEAGUE_CODE {
+  PL = 'PL',
+  FL1 = 'FL1',
+  ELC = 'ELC',
+  BSA = 'BSA',
+  CL = 'CL',
+  EC = 'EC',
+  BL1 = 'BL1',
+  SA = 'SA',
+  DED = 'DED',
+  PPL = 'PPL',
+  PD = 'PD',
+  WC = 'WC',
+}
