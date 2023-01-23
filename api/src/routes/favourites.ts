5c9c2e2c086666b8edd18f859b91dd37d885c3a8
Tom Blaymire <thomasblaymire@gmail.com>
2023-01-23 22:04:34 +0000

    feat(api): add new router files and init prisma on the backend

---
+++ api/src/routes/favourites.ts
@@ -0,0 +1,84 @@
+import express, { Request, Response } from "express";
+import { catchAsync } from "../helpers/async";
+import { prisma } from "../helpers/prisma";
+
+const router = express.Router();
+
+// ADD FAVOURITE MATCH
+// ADD VERIFY_AUTH
+router.post(
+  "/api/favourites",
+  catchAsync(async (req: Request, res: Response) => {
+    const { matchId, userId } = req.body;
+
+    console.log("adding favourite", { matchId: matchId, userId: userId });
+
+    if (!matchId) {
+      return res
+        .status(400)
+        .json({ message: "Unable to add favourite match, plase try again." });
+    }
+
+    try {
+      await prisma.favourites.create({
+        data: {
+          matchId,
+          userId,
+        },
+      });
+      return res.json({ matchId, status: "ADDED" });
+    } catch (e) {
+      console.error(e);
+      return res.status(500).json({ message: "Sorry, something went wrong." });
+    }
+  })
+);
+
+router.delete(
+  "/api/favourites",
+  catchAsync(async (req: Request, res: Response) => {
+    const { matchId, userId } = req.body;
+
+    console.log("removing favourite", { matchId: matchId, userId: userId });
+
+    if (!matchId) {
+      return res.status(400).json({
+        message: "Unable to remove favourite match, plase try again.",
+      });
+    }
+
+    try {
+      await prisma.favourites.delete({
+        where: {
+          id: "dsds",
+        },
+      });
+      return res.json({ matchId, status: "REMOVED" });
+    } catch (e) {
+      console.error(e);
+      return res.status(500).json({ message: "Sorry, something went wrong." });
+    }
+  })
+);
+
+// GET FAVOURITE MATCHES
+// REQUIRE AUTH
+router.get(
+  "/api/favourites",
+  catchAsync(async (req: Request, res: Response) => {
+    const { userId } = req.body;
+    try {
+      const favourites = await prisma.favourites.findMany({
+        where: {
+          userId,
+        },
+      });
+      return res.json(favourites);
+    } catch (err) {
+      console.error(err);
+      return res.status(500).json({ message: "Sorry, something went wrong." });
+    }
+  })
+);
+
+export { router as favouritesRouter };
---
+++ api/src/routes/search.ts
@@ -0,0 +1,18 @@
+import express, { Request, Response } from "express";
+import { catchAsync } from "../helpers/async";
+
+const router = express.Router();
+
+router.get(
+  "/api/search",
+  catchAsync(async (req: Request, res: Response) => {
+    const { searchTerm } = req.query;
+    console.log("debug searchTerm", searchTerm);
+
+    const response = await fetch(process.env.MOCKY_SEARCH_API_URL);
+    const data = await response.json();
+    return res.json(data);
+  })
+);
+
+export { router as searchRouter };
