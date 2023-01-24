51b324d22235f348fa79c39fed923aac854e0773
Tom Blaymire <thomasblaymire@gmail.com>
2023-01-24 21:16:20 +0000

    feat(client): implement a basic footer component

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -8,6 +8,7 @@ import { CompetitionList } from '../components/competition-list'
 import { StandingsTable } from '../components/standings'
 import { BetCard } from '../components/bet-card'
 import { Card } from '../components/card'
+import { Footer } from '../components/footer'
 import { getStandings } from '../lib/api-helpers'
 import { Competition } from '../types/index'
 
@@ -20,82 +21,84 @@ export default function Home({ competitions, league }: HomeProps) {
   const [isTablet] = useMediaQuery('(min-width: 780px)')
 
   return (
-    <Box width="1200px" margin="0 auto">
-      <Grid
-        marginTop={{ base: '1rem', md: '2rem' }}
-        columnGap="2.4rem"
-        rowGap="2.4rem"
-        height="1000px"
-        templateAreas={{
-          base: `"sidebar main" ". aside"`,
-          sm: `"sidebar main"`,
-          md: `"sidebar main aside"`,
-        }}
-        gridTemplateRows="auto"
-        // gridTemplateColumns={{
-        //   base: 'minmax(0,5fr) minmax(0,12fr) minmax(300px,7fr)',
-        //   xs: 'minmax(0,1fr)',
-        //   md: 'minmax(0,5fr) minmax(0,12fr) minmax(300px,7fr)',
-        // }}
-        gridTemplateColumns={{
-          sm: 'minmax(0,6fr) minmax(0,12fr) minmax(300px,7fr)',
-          base: 'minmax(0,1fr)',
-        }}
-      >
-        {isTablet && (
-          <GridItem area={'sidebar'}>
-            <Sidebar>
-              <Card
-                color="#FFF"
-                heading="Top Competitions"
-                background="#1F1F1F"
-                margin="0 0 2rem 0"
-                height="45vh"
-                radius="15px"
-              >
-                <CompetitionList competitions={competitions} />
-              </Card>
-            </Sidebar>
-          </GridItem>
-        )}
+    <>
+      <Box width="1200px" margin="0 auto" height="100%" mb="4rem">
+        <Grid
+          marginTop={{ base: '1rem', md: '2rem' }}
+          columnGap="2.4rem"
+          rowGap="2.4rem"
+          templateAreas={{
+            base: `"sidebar main" ". aside"`,
+            sm: `"sidebar main"`,
+            md: `"sidebar main aside"`,
+          }}
+          gridTemplateRows="auto"
+          // gridTemplateColumns={{
+          //   base: 'minmax(0,5fr) minmax(0,12fr) minmax(300px,7fr)',
+          //   xs: 'minmax(0,1fr)',
+          //   md: 'minmax(0,5fr) minmax(0,12fr) minmax(300px,7fr)',
+          // }}
+          gridTemplateColumns={{
+            sm: 'minmax(0,6fr) minmax(0,12fr) minmax(300px,7fr)',
+            base: 'minmax(0,1fr)',
+          }}
+        >
+          {isTablet && (
+            <GridItem area={'sidebar'}>
+              <Sidebar>
+                <Card
+                  color="#FFF"
+                  heading="Top Competitions"
+                  background="#1F1F1F"
+                  margin="0 0 2rem 0"
+                  height="45vh"
+                  radius="15px"
+                >
+                  <CompetitionList competitions={competitions} />
+                </Card>
+              </Sidebar>
+            </GridItem>
+          )}
 
-        <GridItem area={'main'}>
-          <BetCard />
-          <Heading
-            fontSize="1.3rem"
-            color="white"
-            marginBottom="1rem"
-            fontFamily="inherit"
-          >
-            Football Matches
-          </Heading>
-          <ScoreBoard />
-        </GridItem>
-
-        {isTablet && (
-          <GridItem area={'aside'}>
-            <Sidebar>
-              <Card
-                background="#121212"
-                margin="0 0 2rem 0"
-                height="45vh"
-                radius="15px"
-              />
-              <Card
-                heading="Standings"
-                background="#121212"
-                color="#FFF"
-                radius="15px"
-              >
-                <Box padding="0 1rem">
-                  <StandingsTable standings={league.standings} size="sm" />
-                </Box>
-              </Card>
-            </Sidebar>
+          <GridItem area={'main'}>
+            <BetCard />
+            <Heading
+              fontSize="1.3rem"
+              color="white"
+              marginBottom="1rem"
+              fontFamily="inherit"
+            >
+              Football Matches
+            </Heading>
+            <ScoreBoard />
           </GridItem>
-        )}
-      </Grid>
-    </Box>
+
+          {isTablet && (
+            <GridItem area={'aside'}>
+              <Sidebar>
+                <Card
+                  background="#121212"
+                  margin="0 0 2rem 0"
+                  height="45vh"
+                  radius="15px"
+                />
+                <Card
+                  heading="Standings"
+                  background="#121212"
+                  color="#FFF"
+                  radius="15px"
+                >
+                  <Box padding="0 1rem">
+                    <StandingsTable standings={league.standings} size="sm" />
+                  </Box>
+                </Card>
+              </Sidebar>
+            </GridItem>
+          )}
+        </Grid>
+      </Box>
+      <Footer />
+    </>
   )
 }
 
