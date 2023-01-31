bb49c4d3db7e823fc7a743c1feb6be8877fa68c3
Tom Blaymire <thomasblaymire@gmail.com>
2023-01-31 12:57:03 +0000

    feat(client): refactor home styles and data from api

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,5 +1,5 @@
 import Sidebar from '../components/sidebar'
-import { Box, Flex, Container, Grid, GridItem } from '@chakra-ui/layout'
+import { Box, Grid, GridItem } from '@chakra-ui/layout'
 import { dehydrate, QueryClient } from '@tanstack/react-query'
 import { ScoreBoard } from '../components/scoreboard'
 import { useMediaQuery, Heading } from '@chakra-ui/react'
@@ -9,50 +9,52 @@ import { StandingsTable } from '../components/standings'
 import { BetCard } from '../components/bet-card'
 import { Card } from '../components/card'
 import { Footer } from '../components/footer'
-import { getStandings } from '../lib/api-helpers'
-import { Competition } from '../types/index'
+import { getLeague } from '../lib/api-helpers'
 
 interface HomeProps {
-  competitions: Competition[]
-  league: any
+  competitions: Competitions[]
+  competition: Competition
 }
 
-export default function Home({ competitions, league }: HomeProps) {
+export default function Home({ competitions, competition }: HomeProps) {
   const [isTablet] = useMediaQuery('(min-width: 780px)')
 
   return (
     <>
-      <Box width="1200px" margin="0 auto" height="100%" mb="4rem">
+      <Box
+        width={{
+          md: '720px',
+          lg: '960px',
+          xl: '1200px',
+        }}
+        maxWidth="unset"
+        margin="0 auto"
+        height="100%"
+        mb="4rem"
+      >
         <Grid
           marginTop={{ base: '1rem', md: '2rem' }}
           columnGap="2.4rem"
           rowGap="2.4rem"
           templateAreas={{
-            base: `"sidebar main" ". aside"`,
-            sm: `"sidebar main"`,
-            md: `"sidebar main aside"`,
+            base: `"main"`,
+            md: `"sidebar main" ". aside"`,
+            xl: `"sidebar main aside"`,
           }}
           gridTemplateRows="auto"
-          // gridTemplateColumns={{
-          //   base: 'minmax(0,5fr) minmax(0,12fr) minmax(300px,7fr)',
-          //   xs: 'minmax(0,1fr)',
-          //   md: 'minmax(0,5fr) minmax(0,12fr) minmax(300px,7fr)',
-          // }}
           gridTemplateColumns={{
-            sm: 'minmax(0,6fr) minmax(0,12fr) minmax(300px,7fr)',
             base: 'minmax(0,1fr)',
+            md: 'minmax(0,7fr) minmax(0,17fr) ',
+            xl: 'minmax(0,6fr) minmax(0,12fr) minmax(300px,7fr)',
           }}
         >
           {isTablet && (
             <GridItem area={'sidebar'}>
               <Sidebar>
                 <Card
-                  color="#FFF"
                   heading="Top Competitions"
-                  background="#1F1F1F"
                   margin="0 0 2rem 0"
                   height="45vh"
-                  radius="15px"
                 >
                   <CompetitionList competitions={competitions} />
                 </Card>
@@ -76,20 +78,15 @@ export default function Home({ competitions, league }: HomeProps) {
           {isTablet && (
             <GridItem area={'aside'}>
               <Sidebar>
-                <Card
-                  background="#121212"
-                  margin="0 0 2rem 0"
-                  height="45vh"
-                  radius="15px"
-                />
-                <Card
-                  heading="Standings"
-                  background="#121212"
-                  color="#FFF"
-                  radius="15px"
-                >
+                <Card margin="0 0 2rem 0" height="45vh" heading="Latest News">
+                  News
+                </Card>
+                <Card heading="Standings">
                   <Box padding="0 1rem">
-                    <StandingsTable standings={league.standings} size="sm" />
+                    <StandingsTable
+                      standings={competition.standings}
+                      size="sm"
+                    />
                   </Box>
                 </Card>
               </Sidebar>
@@ -104,19 +101,18 @@ export default function Home({ competitions, league }: HomeProps) {
 
 // Build time fetch leagues and inital score data
 export async function getStaticProps() {
-  const competitions = await getCompetitions()
   const queryClient = new QueryClient()
+
+  const competitions = await getCompetitions()
+  const competition = await getLeague('39')
+
   await queryClient.prefetchQuery(['matches'], getMatches)
-
-  const data = await getStandings('39')
-
-  console.log('server side ', data.standings[0].league)
 
   return {
     props: {
       dehydratedState: dehydrate(queryClient),
       competitions: competitions,
-      league: data.standings[0].league,
+      competition: competition.league,
     },
   }
 }
