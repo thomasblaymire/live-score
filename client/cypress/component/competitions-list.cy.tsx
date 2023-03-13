bde0c8e897faf67c9044fa37e67754aa921282fd
Tom Blaymire <thomasblaymire@gmail.com>
2023-03-13 18:48:11 +0000

    test(app): update cypress testing setup

---
+++ client/cypress/component/competitions-list.cy.tsx
@@ -0,0 +1,24 @@
+import { CompetitionList } from '../../components/competition-list'
+import { competitionData } from '../fixtures/competitions'
+
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+const queryClient = new QueryClient()
+
+describe('Competitions list component', () => {
+  const competitionList = '[data-test=competition-list]'
+
+  beforeEach(() => {
+    cy.mount(
+      <QueryClientProvider client={queryClient}>
+        <CompetitionList competitions={competitionData} />
+      </QueryClientProvider>
+    )
+  })
+
+  it('should render a list of different competitions with images', () => {
+    cy.get(competitionList).should('exist')
+    cy.get(competitionList).find('li').should('have.length.greaterThan', 5)
+    cy.get(competitionList).find('img').should('be.visible')
+  })
+})
