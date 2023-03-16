9add4b46b88ff3845530323bdb5235805f47a593
Tom Blaymire <thomasblaymire@gmail.com>
2023-03-16 20:25:36 +0000

    test(app): add in some base cypress testing for search

--- client/cypress/e2e/search.cy.ts
+++ client/cypress/e2e/search.cy.ts
@@ -6,6 +6,47 @@ describe('Search', () => {
     cy.visit('http://localhost:3000/')
   })
 
+  beforeEach(() => {
+    // Visit the page with the search component
+    cy.visit('/search')
+  })
+
+  it('displays search results for a valid query', () => {
+    // Type a search query into the search input
+    cy.get('input[type="search"]').type('cypress')
+
+    // Click the search button
+    cy.get('button[type="submit"]').click()
+
+    // Check that search results are displayed
+    cy.get('.search-results').should('exist')
+  })
+
+  it('displays a message for an empty search', () => {
+    // Leave the search input empty
+    cy.get('input[type="search"]').clear()
+
+    // Click the search button
+    cy.get('button[type="submit"]').click()
+
+    // Check that an error message is displayed
+    cy.get('.error-message').should('contain', 'Please enter a search term')
+  })
+
+  it('displays an error message for an invalid search', () => {
+    // Type an invalid search query into the search input
+    cy.get('input[type="search"]').type('qwertyuiopasdfghjklzxcvbnm')
+
+    // Click the search button
+    cy.get('button[type="submit"]').click()
+
+    // Check that an error message is displayed
+    cy.get('.error-message').should(
+      'contain',
+      'No results found for search term'
+    )
+  })
+
   it('should not display a search icon when a user is not authenticated', () => {})
 
   it('should display a fullpage search when a user clicks on the search icon', () => {
