429f2f90257e46877b5f571b186820172d96b8ef
Tom Blaymire <thomasblaymire@gmail.com>
2023-03-19 17:35:35 +0000

    feat(app): create a hlper for parsing url strings

--- client/lib/time.ts
+++ client/lib/time.ts
@@ -1,6 +1,16 @@
-export const parse = (time: string): number => {
+const parse = (time: string): number => {
   const [hours, minutes, seconds] = time
     .split(':')
     .map((unit) => parseFloat(unit))
   return hours * 3600 + minutes * 60 + seconds
 }
+
+// Takes a UTC and returns a formatted date/time
+// e.g 2019-12-26T17:30:00+00:00 => Thu, 26 Dec 2019
+const formatUTCDate = (date: Date): string => {
+  let value = new Date(date).toUTCString()
+  value = value.split(' ').slice(0, 5).join(' ')
+  return value
+}
+
+export { parse, formatUTCDate }
