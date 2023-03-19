b0c713be0da7c5dab49b8d98c88efc4197837d2a
Tom Blaymire <thomasblaymire@gmail.com>
2023-03-19 17:34:05 +0000

    feat(api): update comment on fetch helper

--- api/src/helpers/async.ts
+++ api/src/helpers/async.ts
@@ -1,8 +1,9 @@
 import express, { Request, Response, NextFunction } from "express";
 
-// Helper to catch async errors vs multiple try/catch blocks
 export const catchAsync = (fn: any) => {
   return (req: Request, res: Response, next: NextFunction) => {
     fn(req, res, next).catch(next);
   };
 };
+
+// Helper to catch async errors vs multiple try/catch blocks
