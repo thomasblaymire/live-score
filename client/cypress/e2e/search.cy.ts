5375e54735fb768d3db7710d0f93b71cd4cf7646
Tom Blaymire <thomasblaymire@gmail.com>
2023-03-21 14:58:07 +0000

    feat(app): add some additional unit tests

--- client/cypress/e2e/search.cy.ts
+++ client/cypress/e2e/search.cy.ts
@@ -2,13 +2,39 @@ describe('Search', () => {
   const searchInput = '[data-test="search-input"]'
   const searchButton = '[data-test="search-button"]'
 
-  beforeEach(() => {
-    cy.visit('http://localhost:3000/')
-  })
+  // beforeEach(() => {
+  //   cy.visit('http://localhost:3000/')
+  //   cy.intercept('/api/auth/signin', (req) => {
+  //     const session: any = {
+  //       user: {
+  //         name: 'John Doe',
+  //         email: 'johndoe@example.com',
+  //         image: 'https://example.com/profile.jpg',
+  //       },
+  //       expires: '2023-03-20T00:00:00.000Z',
+  //     }
+  //     req.reply(session)
+  //   })
+  //   cy.wait(500)
+  // })
 
   beforeEach(() => {
-    // Visit the page with the search component
-    cy.visit('/search')
+    cy.visit('http://localhost:3000/')
+    cy.intercept('POST', '/auth/signin', (req) => {
+      // Set the authentication token in the response
+      req.reply({
+        status: 200,
+        body: {
+          token: 'fake-token',
+        },
+      })
+    })
+    cy.visit('/auth/signin')
+
+    // Fill in the login form and submit it
+    cy.get('[data-test=signin-input-email]').type('testuser')
+    cy.get('[data-test=signin-input-password]').type('password')
+    cy.get('form').submit()
   })
 
   it('displays search results for a valid query', () => {
@@ -59,6 +85,35 @@ describe('Search', () => {
     cy.get(searchButton).click()
     cy.get(searchInput).clear().type('leeds united')
     cy.get('[data-test="search-result"]').should('have.length', 1)
+  })
+
+  it('should highlight the first result when the user types in text and tabs through the results', () => {
+    cy.get('[data-test="search-input"]').type('search term{tab}')
+    cy.get('[data-test="search-result"]')
+      .first()
+      .should('have.css', 'background-color', 'rgb(49, 49, 49)')
+  })
+
+  it('should highlight the second result when the user types in text and tabs through the results twice', () => {
+    cy.get('[data-test="search-input"]').type('search term{tab}{tab}')
+    cy.get('[data-test="search-result"]')
+      .eq(1)
+      .should('have.css', 'background-color', 'rgb(49, 49, 49)')
+  })
+
+  it('should wrap around to the first result when the user tabs from the last result', () => {
+    cy.get('[data-test="search-input"]').type('search term{tab}{tab}{tab}')
+    cy.get('[data-test="search-result"]')
+      .first()
+      .should('have.css', 'background-color', 'rgb(49, 49, 49)')
+  })
+
+  it('should not highlight any results when the user has not typed in text', () => {
+    cy.get('[data-test="search-result"]').should(
+      'have.css',
+      'background-color',
+      'rgb(255, 255, 255)'
+    )
   })
 })
 
