b0b40615386618100ddc4e4a678f3f0df2978412
Tom Blaymire <thomasblaymire@gmail.com>
2023-03-22 17:02:51 +0000

    feat(api): add pagination to news route

--- api/src/routes/news.ts
+++ api/src/routes/news.ts
@@ -8,10 +8,43 @@ router.get(
   "/api/news",
   catchAsync(async (req: Request, res: Response) => {
     const response = await fetch(
-      `https://newsapi.org/v2/top-headlines?country=gb&category=sports&apiKey=${process.env.FOOTBALL_NEWS_API_KEY}`
+      `https://newsapi.org/v2/top-headlines?q=football&country=gb&category=sports&apiKey=${process.env.FOOTBALL_NEWS_API_KEY}`
     );
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch data from API: ${response.statusText}`);
+    }
+
     const data = await response.json();
     res.json(data);
+  })
+);
+
+router.post(
+  "/api/news/:team",
+  catchAsync(async (req: Request, res: Response) => {
+    const { team } = req.params;
+    const page = parseInt(req.query.page as string, 10) || 1;
+    const limit = parseInt(req.query.limit as string, 10) || 5;
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+
+    const response = await fetch(
+      `https://newsapi.org/v2/top-headlines?q=${team}&country=gb&category=sports&apiKey=${process.env.FOOTBALL_NEWS_API_KEY}`
+    );
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch data from API: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    const paginatedData = data.articles.slice(startIndex, endIndex);
+
+    res.json({
+      currentPage: page,
+      totalPages: Math.ceil(data.length / limit),
+      data: paginatedData,
+    });
   })
 );
 
