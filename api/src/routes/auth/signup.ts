54c1889b4803230678b923d498ac553861beced6
Tom Blaymire <thomasblaymire@gmail.com>
2023-04-03 22:00:31 +0100

    feat(api): refactor nodejs authentication

--- api/src/routes/auth/signup.ts
+++ api/src/routes/auth/signup.ts
@@ -1,7 +1,6 @@
 import fetch from "node-fetch";
-import express, { Request, response, Response } from "express";
+import express, { Request, Response } from "express";
 import jwt from "jsonwebtoken";
-import cookie from "cookie";
 import bcrypt from "bcrypt";
 import { prisma } from "../../helpers/prisma";
 import { catchAsync } from "../../helpers/async";
@@ -11,50 +10,51 @@ const router = express.Router();
 router.post(
   "/api/signup",
   catchAsync(async (req: Request, res: Response) => {
-    const salt = bcrypt.genSaltSync();
     const { email, password, name } = req.body;
 
-    console.log("debug", { email, password, name });
+    if (!email || !password) {
+      return res
+        .status(400)
+        .send({ message: "Email and password are required." });
+    }
+
+    const salt = bcrypt.genSaltSync();
 
     let user;
 
     try {
       user = await prisma.user.create({
         data: {
+          name,
           email,
           password: bcrypt.hashSync(password, salt),
-          name,
         },
       });
-    } catch (err) {
-      res.status(401);
-      res.json({ error: "User already exists" });
-      return;
+    } catch (err: any & { code?: string }) {
+      if (err.code === "P2002") {
+        return res.status(400).send({ message: "Email is already taken." });
+      }
+      console.error(err);
+      return res.status(500).send({ message: "Internal server error." });
     }
 
     const token = jwt.sign(
       {
         email: user.email,
-        password: user.id,
+        name: user.name,
+        id: user.id,
         time: Date.now(),
       },
       process.env.JWT_SECRET,
       { expiresIn: "8h" }
     );
 
-    // Sets cookie on browser
-    res.setHeader(
-      "Set-Cookie",
-      cookie.serialize("LIVE_SCORE_ACCESS_TOKEN", token, {
-        httpOnly: true, // not accessible by JS
-        maxAge: 8 * 60 * 60,
-        path: "/",
-        sameSite: "lax",
-        secure: process.env.NODE_ENV === "production",
-      })
-    );
+    res.cookie("token", token, { httpOnly: true, maxAge: 8 * 60 * 60 });
 
-    res.json(user);
+    return res.status(201).send({
+      user: { id: user.id, email: user.email, name: user.name },
+      token,
+    });
   })
 );
 
--- api/src/routes/search.ts
+++ api/src/routes/search.ts
@@ -8,11 +8,17 @@ router.get(
   "/api/search",
   catchAsync(async (req: Request, res: Response) => {
     const { searchTerm } = req.query;
-    console.log("debug searchTerm", searchTerm);
-
-    const response = await fetch(process.env.MOCKY_SEARCH_API_URL);
-    const data = await response.json();
-    return res.json(data);
+    try {
+      const response = await fetch(process.env.MOCKY_SEARCH_API_URL);
+      const data = await response.json();
+      if (data.length === 0) {
+        return res.status(404).json({ message: "No results found." });
+      }
+      return res.json(data);
+    } catch (err) {
+      console.error("Failed to fetch search results:", err);
+      return res.status(500).json({ error: "Failed to fetch search results." });
+    }
   })
 );
 
