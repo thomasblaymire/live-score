c819ae14870f3cdf2d86e72401044cd65ef8f4c8
Tom Blaymire <thomasblaymire@gmail.com>
2023-04-06 22:07:41 +0100

    feat(api): update auth to pass userid on req

--- api/src/middlewares/auth.ts
+++ api/src/middlewares/auth.ts
@@ -1,9 +1,8 @@
 import { Request, Response, NextFunction, RequestHandler } from "express";
+import jwt from "jsonwebtoken";
 
-interface AuthenticatedRequest extends Request {
-  session?: {
-    userId: string;
-  };
+export interface AuthenticatedRequest extends Request {
+  userId?: string;
 }
 
 export const verifyAuth: RequestHandler = (
@@ -11,11 +10,24 @@ export const verifyAuth: RequestHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  if (req.session && req.session.userId) {
-    // User is authenticated, allow access
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ message: "Authorization header missing." });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decodedToken = jwt.verify(
+      token,
+      process.env.JWT_SECRET as string
+    ) as { id: string };
+
+    // Attach the userId to the request object
+    req.userId = decodedToken.id;
     next();
-  } else {
-    // Redirect to login page
-    res.status(401).json({ message: "Authorization failed." });
+  } catch (error) {
+    res.status(401).json({ message: "Invalid token." });
   }
 };
