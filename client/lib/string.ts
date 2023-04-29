6be9d3fdaecb3cd43074f364c04b3168498e28f0
Tom Blaymire <thomasblaymire@gmail.com>
2023-04-29 10:50:38 +0100

    feat(app): neaten up string file with comments

--- client/lib/string.ts
+++ client/lib/string.ts
@@ -1,14 +1,30 @@
-export const hypenateMatchString = (
+/**
+ * Creates a hyphenated match string from two given strings.
+ * @param firstString - The first string
+ * @param secondString - The second string
+ * @returns A hyphenated match string in the format "firstString-vs-secondString"
+ */
+export const hyphenateMatchString = (
   firstString: string,
   secondString: string
 ): string => {
   return `${firstString}-vs-${secondString}`.replace(/\s+/g, '-').toLowerCase()
 }
 
-export const hypenate = (string: string): string => {
-  return `${string}`.replace(/\s+/g, '-').toLowerCase()
+/**
+ * Replaces spaces with hyphens and converts the given string to lowercase.
+ * @param string - The input string
+ * @returns The modified string with spaces replaced by hyphens and in lowercase
+ */
+export const hyphenate = (string: string): string => {
+  return string.replace(/\s+/g, '-').toLowerCase()
 }
 
+/**
+ * Formats a slug by capitalizing the first letter of each word and replacing underscores with spaces.
+ * @param slug - The input slug
+ * @returns The formatted slug
+ */
 export const formatSlug = (slug: string): string => {
   const formattedWord = (word: string) =>
     word?.charAt(0).toUpperCase() + word?.slice(1)
