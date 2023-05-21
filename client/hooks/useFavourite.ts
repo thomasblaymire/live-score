7cea22ef0374a6ada6ed5e12ba96a0cb1663d4fd
Tom Blaymire <thomasblaymire@gmail.com>
2023-05-21 22:52:42 +0100

    feat(client): refactor useFavourite hook to deal with add/load/remove also support a map of results

--- client/hooks/useFavourite.ts
+++ client/hooks/useFavourite.ts
@@ -1,55 +1,148 @@
-import { useState } from 'react'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { API_URL } from '@/lib/constants'
 
 interface Favourite {
   userId: string | undefined
+  favItems: FavouriteItem[]
+  initialIsFavoritedMap: Record<string, boolean>
+}
+
+interface FavouriteItem {
   favType: string
-  favTypeId: string | number
-  initialIsFavorited: boolean
+  favTypeId: number
+}
+
+interface FavouriteStatusResponse {
+  userId: string
+  favItems: FavouriteItem[]
 }
 
 interface useFavouriteResponse {
+  isFavoritedMap: Record<string, boolean>
+  toggleFavorite: (
+    favType: string,
+    favTypeId: number,
+    isFavorited: boolean
+  ) => void
+}
+
+interface MutationVariables extends FavouriteItem {
+  userId: string | undefined
   isFavorited: boolean
-  toggleFavorite: () => void
 }
 
 export const useFavourite = ({
   userId,
-  favType,
-  favTypeId,
-  initialIsFavorited,
+  favItems,
+  initialIsFavoritedMap,
 }: Favourite): useFavouriteResponse => {
   const queryClient = useQueryClient()
-  const [isFavorited, setFavorited] = useState(initialIsFavorited)
+  const [isFavoritedMap, setFavorited] = useState(initialIsFavoritedMap)
 
-  const toggleFavoriteMutation = useMutation(
-    () =>
-      fetch(`${API_URL}/favourites`, {
-        method: isFavorited ? 'DELETE' : 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          userId,
-          favType,
-          favTypeId,
-        }),
-      }),
+  // Effect to fetch initial favourite status
+  useEffect(() => {
+    const fetchInitialFavStatus = async () => {
+      try {
+        const { data } = await axios.post(
+          `${API_URL}/favourites/status`,
+          {
+            userId,
+            favItems,
+          },
+          {
+            withCredentials: true,
+          }
+        )
+
+        // If data is an object like { "Team_63": true }, use it directly
+        if (typeof data === 'object' && !Array.isArray(data)) {
+          setFavorited(data)
+        } else if (Array.isArray(data.favItems)) {
+          // Otherwise, if data has a favItems array, create a map from it
+          const newFavoritedMap = favItems.reduce((acc, item) => {
+            acc[`${item.favType}_${item.favTypeId}`] = Boolean(
+              data.favItems.find(
+                (favItem: any) =>
+                  favItem.favType === item.favType &&
+                  favItem.favTypeId === item.favTypeId
+              )
+            )
+            return acc
+          }, {} as Record<string, boolean>)
+
+          setFavorited(newFavoritedMap)
+        } else {
+          console.error('Unexpected response data structure', data)
+        }
+      } catch (error) {
+        console.error('Failed to fetch initial favorite status', error)
+      }
+    }
+
+    fetchInitialFavStatus()
+  }, [userId])
+
+  const toggleFavoriteMutation = useMutation<
+    any,
+    unknown,
+    MutationVariables,
+    unknown
+  >(
+    async ({ isFavorited, ...variables }) => {
+      // Depending on the current favourite status, send a POST or DELETE request
+      if (isFavorited) {
+        const response = await axios.delete(`${API_URL}/favourites`, {
+          data: { ...variables },
+          withCredentials: true,
+        })
+        return response.data
+      } else {
+        const response = await axios.post(
+          `${API_URL}/favourites`,
+          {
+            ...variables,
+          },
+          {
+            withCredentials: true,
+          }
+        )
+        return response.data
+      }
+    },
     {
-      onError: (error: any) => {
-        console.error('Failed to toggle favorite', error)
+      onError: () => {
+        console.error('Failed to toggle favorite')
       },
       onSettled: () => {
+        // Invalidate queries to refetch the updated favourite status
         queryClient.invalidateQueries({ queryKey: ['favorite'] })
       },
     }
   )
 
-  const toggleFavorite = () => {
-    setFavorited((prevFavorited: any) => !prevFavorited)
-    toggleFavoriteMutation.mutate()
+  const toggleFavorite = (
+    favType: string,
+    favTypeId: number,
+    isFavorited: boolean
+  ) => {
+    // Update the local favourite status map
+    const newFavoritedMap = {
+      ...isFavoritedMap,
+      [`${favType}_${favTypeId}`]: isFavorited,
+    }
+    setFavorited(newFavoritedMap)
+
+    // Send a mutation request to update favourite status in the server
+    toggleFavoriteMutation.mutate({
+      userId,
+      favType,
+      favTypeId,
+      isFavorited: !isFavorited,
+    })
   }
 
-  return { isFavorited, toggleFavorite }
+  // Return the map of favourite status and the function to toggle favourite
+  return { isFavoritedMap, toggleFavorite }
 }
