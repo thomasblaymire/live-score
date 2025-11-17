577b7e774ccc3f56b8d940505027a598763490ab
Tom Blaymire <thomasblaymire@gmail.com>
2025-11-17 21:45:50 +0000

    chore: remove legacy directory

--- apps/legacy/client/pages/news.tsx
+++
@@ -1,3 +0,0 @@
-export default function News() {
-  return <h2>News Page</h2>
-}
--- apps/legacy/frontend/src/lib/generators.ts
+++
@@ -1,4 +0,0 @@
-// We can pull in uuid if required in the future
-export const uniqueId = () => {
-  return Math.random().toString(36).substr(2, 9)
-}
